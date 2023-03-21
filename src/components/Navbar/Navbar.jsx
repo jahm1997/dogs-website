ee559a9e80e5ddc0be9629c32fd17c07d70ca7af
jahm1997 <jahm1997@gmail.com>
2023-03-21 18:34:05 -0500

    solucion navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,22 +11,22 @@ const Navbar = () => {
   return (
     <div className={style.menu} >
           <div className={style.logo} >
-              <a href="/">
+              <a href="/dogs-website/">
                 <img src={logoTipo} alt={logoTipo} />  
               </a>
           </div>
           
-          { location.pathname === "/dogs" ? <Filter/> : <h1>¡ME ESTA QUEDANDO CHIDO!</h1> }
+          { location.pathname === "/dogs-website/dogs" ? <Filter/> : <h1>¡ME ESTA QUEDANDO CHIDO!</h1> }
           
           
           <div className={style.homePage} >
-              <a href="/dogs">
+              <a href="dogs-website/dogs">
                 <img src={homePage} alt={homePage} />
               </a>
           </div>
 
           <div className={style.crearDog} >
-              <a href="/dogs/add">
+              <a href="/dogs-website/dogs/add">
                 <img src={CrearDog} alt={CrearDog} />
               </a>
               {/* <Link to="/dogs/add">Create Dog</Link> */}
