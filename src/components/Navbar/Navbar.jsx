910d72349ba965f8080a7701097a8c223d02733a
jahm1997 <jahm1997@gmail.com>
2023-03-22 11:33:29 -0500

    10mo commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import Filter from "../Filters/Filter"
 import style from "./Navbar.module.css";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import homePage from "../imagenes/homePage.jpg"
 import CrearDog from "../imagenes/createDog.jpg"
 import logoTipo from "../imagenes/logoTipo_dogs_modified.png"
@@ -11,24 +11,24 @@ const Navbar = () => {
   return (
     <div className={style.menu} >
           <div className={style.logo} >
-              <a href="/">
+              <Link to ="/">
                 <img src={logoTipo} alt={logoTipo} />  
-              </a>
+              </Link>
           </div>
           
           { location.pathname === "/dogs" ? <Filter/> : <h1>¡ME ESTA QUEDANDO CHIDO!</h1> }
           
           
           <div className={style.homePage} >
-              <a href="/dogs">
+              <Link to="/dogs">
                 <img src={homePage} alt={homePage} />
-              </a>
+              </Link>
           </div>
 
           <div className={style.crearDog} >
-              <a href="/dogs/add">
+              <Link to="/dogs/add">
                 <img src={CrearDog} alt={CrearDog} />
-              </a>
+              </Link>
               {/* <Link to="/dogs/add">Create Dog</Link> */}
           </div>
     </div>
