8415a8f50e5870529f55cb0d7dbc5a4db0d68ba9
jahm1997 <jahm1997@gmail.com>
2023-03-21 19:23:59 -0500

    cambio minimo basename kick

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <BrowserRouter basename="/dogs-website">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
